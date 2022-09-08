ec5ea4555c05aaa455ae700dc49c8a5e4231ddf8
ColdFireB <86348290+ColdFireB@users.noreply.github.com>
2022-09-08 22:32:30 +0530

    Done
    done

---
+++ PracticalPrograms/src/com/rationCard/SortByAge.java
@@ -0,0 +1,8 @@
+package com.rationCard;
+import java.util.Comparator;
+
+class SortByAge implements Comparator<RationCard> {
+	public int compare(RationCard a, RationCard b) {
+		return a.age - b.age;
+	}
+}
---
+++ PracticalPrograms/src/com/rationCard/SortByRoll.java
@@ -0,0 +1,8 @@
+package com.rationCard;
+import java.util.Comparator;
+
+class SortByRoll implements Comparator<RationCard> {
+	public int compare(RationCard a, RationCard b) {
+		return a.rollnum - b.rollnum;
+	}
+}
